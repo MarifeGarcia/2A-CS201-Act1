7b686298ddeb2b5df32cb64e9ba301a7521d381d
MarifeGarcia <147678876+MarifeGarcia@users.noreply.github.com>
2023-10-18 15:15:28 +0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,14 @@
+let firstFavNum = 4, secondFavNum = 20; 
+  
+ console.log(firstFavNum + secondFavNum); 
+ 
+ console.log(firstFavNum - secondFavNum); 
+ 
+ console.log(firstFavNum * secondFavNum); 
+ 
+ console.log(firstFavNum / secondFavNum); 
+ 
+ console.log(firstFavNum % secondFavNum); 
+
+ let power = Math.pow(firstFavNum); 
+ console.log(firstFavNum);
